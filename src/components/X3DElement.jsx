78ddaacbd1a522fb8ce0cd04eaae8b246a2b256d
otokama <flynn98721@gmail.com>
2026-02-26 09:57:00 -0500

    feat: use sr-only span

--- src/components/X3DElement.jsx
+++ src/components/X3DElement.jsx
@@ -82,22 +82,16 @@ class X3DElement extends Component {
             </scene>
             <div className="controls">
               <div className="tooltip-wrapper">
-                <button
-                  onClick={this.zoomIn}
-                  type="button"
-                  aria-label="Zoom in"
-                >
+                <button onClick={this.zoomIn}>
                   <i className="far fa-plus-circle"></i>
+                  <span className="sr-only">Zoom in</span>
                 </button>
                 <span className="viewer-tooltip">Zoom In</span>
               </div>
               <div className="tooltip-wrapper">
-                <button
-                  onClick={this.zoomOut}
-                  type="button"
-                  aria-label="Zoom out"
-                >
+                <button onClick={this.zoomOut}>
                   <i className="far fa-minus-circle"></i>
+                  <span className="sr-only">Zoom out</span>
                 </button>
                 <span className="viewer-tooltip">Zoom Out</span>
               </div>
