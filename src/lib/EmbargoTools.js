bf21b698f2e592a61a6700aab36728cc453866ae
Lee Hunter <whunter@vt.edu>
2026-02-27 16:03:44 -0500

    remove references to legacy Embargo table

--- src/lib/EmbargoTools.js
+++ src/lib/EmbargoTools.js
@@ -1,49 +1,9 @@
-import { API, graphqlOperation } from "aws-amplify";
-import * as queries from "../graphql/queries";
-import * as mutations from "../graphql/mutations";
-
-export const validEmbargo = item => {
+export const validEmbargo = (item) => {
   return !!item["embargo_start_date"] || !!item["embargo_end_date"];
 };
 
-export const loadEmbargo = async (itemResponse, setEmbargo) => {
-  if (itemResponse && itemResponse.id) {
-    try {
-      const response = await API.graphql(
-        graphqlOperation(queries.getEmbargo, { id: itemResponse.id })
-      );
-      if (response.data.getEmbargo) {
-        setEmbargo(response.data.getEmbargo);
-        return response.data.getEmbargo;
-      }
-    } catch (error) {
-      console.error("Error fetching embargo", error);
-    }
-  }
-};
-
-export const createEmbargoRecord = async (item, fullItem, embargo, type) => {
-  let embargoMutation = mutations.createEmbargo;
-  if (embargo) {
-    embargoMutation = mutations.updateEmbargo;
-  }
-  const embargoObj = {
-    id: item.id || fullItem.id,
-    identifier: item.identifier || fullItem.identifier,
-    start_date: item.embargo_start_date,
-    end_date: item.embargo_end_date,
-    note: item.embargo_note,
-    record_type: type
-  };
-  await API.graphql({
-    query: embargoMutation,
-    variables: { input: embargoObj },
-    authMode: "AMAZON_COGNITO_USER_POOLS"
-  });
-};
-
-export const toTitleCase = str => {
-  return str.replace(/\w\S*/g, function(txt) {
+export const toTitleCase = (str) => {
+  return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
   });
 };
