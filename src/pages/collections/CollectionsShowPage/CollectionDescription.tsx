af1fe845dfbdfa8a68e59e0e05fcd9239186a2a2
Kelly Ervin Mills <kellyme@vt.edu>
2026-04-15 01:17:08 -0400

    Remove show/hide more description on collection pages

--- src/pages/collections/CollectionsShowPage/CollectionDescription.tsx
+++ src/pages/collections/CollectionsShowPage/CollectionDescription.tsx
@@ -14,44 +14,15 @@ export const CollectionDescription: FC<Props> = ({ description, site }) => {
   if (!description || !description.length) {
     return null;
   }
-  const visibleText = descriptionTruncated
-    ? [description[0].substring(0, TRUNCATION_LENGTH)]
-    : description;
   return (
-    <div
-      className={`description ${descriptionTruncated ? "trunc" : "full"}`}
-      id="collection-description"
-    >
+    <div className="description full" id="collection-description">
       <div>
         {addNewlineInDesc(
-          visibleText,
+          description,
           getDescriptionLabel(
             JSON.parse(site.displayedAttributes),
             "collection"
           )
-        )}{" "}
-        {(description[0].length >= TRUNCATION_LENGTH ||
-          description.length > 1) && (
-          <span>
-            <button
-              onClick={() => setDescriptionTruncated(!descriptionTruncated)}
-              className="more"
-              type="button"
-              aria-controls="collection-description"
-              aria-expanded="false"
-            >
-              . . .[more]
-            </button>
-            <button
-              onClick={() => setDescriptionTruncated(!descriptionTruncated)}
-              className="less"
-              type="button"
-              aria-controls="collection-description"
-              aria-expanded="true"
-            >
-              . . .[less]
-            </button>
-          </span>
         )}
       </div>
     </div>
